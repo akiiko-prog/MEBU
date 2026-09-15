fe4a7e111433507ddd7415e54a6fab149cd32146
akiiko <arthaud.delvau@pm.me>
2026-09-15 20:52:25 +0200

    leger modif sur le graphisme

--- app/(onboarding)/utils/constants.tsx
+++ app/(onboarding)/utils/constants.tsx
@@ -23,11 +23,11 @@ export function GetSupportedServices(redirect: (path: { pathname: string, option
   return [
     {
       name: "university",
-      title: "Auriga (EPITA)",
+      title: "ESME",
       type: "other",
       image: theme.dark ? require("@/assets/images/Auriga_dark.png") : require("@/assets/images/Auriga_light.png"),
       onPress: () => {
-        redirect({ pathname: '../university/multi/aurigaAuth', options: { color: "#0060D6", university: "Microsoft (EPITA)", url: "https://ionisesme-auth.np-auriga.nfrance.net/auth/realms/npionisesme/protocol/openid-connect/auth?client_id=np-front&redirect_uri=https%3A%2F%2Fmy.esme.fr%2F%23%2FmainContent%2Fwelcome&state=b0d51531-8196-40d8-879a-65006e6a077c&response_mode=fragment&response_type=code&scope=openid&nonce=76fd097a-cf70-4f89-8a38-2f7e80b77475&prompt=login&code_challenge=8AH2655_0ZuKl4XeB_TOu0Jbr1HJQoJdPTzG_Rf4Yig&code_challenge_method=S256" } });
+        redirect({ pathname: '../university/multi/aurigaAuth', options: { color: "#0060D6", university: "Microsoft (ESME)", url: "https://ionisesme-auth.np-auriga.nfrance.net/auth/realms/npionisesme/protocol/openid-connect/auth?client_id=np-front&redirect_uri=https%3A%2F%2Fmy.esme.fr%2F%23%2FmainContent%2Fwelcome&state=b0d51531-8196-40d8-879a-65006e6a077c&response_mode=fragment&response_type=code&scope=openid&nonce=76fd097a-cf70-4f89-8a38-2f7e80b77475&prompt=login&code_challenge=8AH2655_0ZuKl4XeB_TOu0Jbr1HJQoJdPTzG_Rf4Yig&code_challenge_method=S256" } });
       },
       variant: 'primary' as const,
       style: { backgroundColor: theme.dark ? colors.border : "black" },
@@ -94,7 +94,7 @@ export function GetNewsServices(redirect: (path: { pathname: string, options?: U
       name: "intracom",
       title: "Intracom",
       type: "main",
-      image: { uri: 'https://intracom.epita.fr/assets/img/logo-epita.png' },
+      image: require("@/assets/images/icon.png"),
       onPress: () => {
         redirect({ pathname: '/(modals)/login-intracom' });
       },
